ff063539f81e1b2b4031247b170a3a8ca618127e
beepbop24 <81869542+beepbop24@users.noreply.github.com>
2025-01-16 14:17:36 -0500

    update simulations

--- simulations.m
+++ simulations.m
@@ -3,32 +3,23 @@ clearvars;
 params = struct();
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
-%settings.plot = false;
 
-[full_sol, I_full, M_full] = full_model("default_params_full.mat", params, @ddefullhist, options, settings, 'default_model');
+full_sol = full_model("default_params_full.mat", params, @ddefullhist, options, settings, 'default_model');
 
-%% DEFAULT RUN FOR FIRST INFECTION WITH NO PREVIOUS IMMUNITY
+%% RUN FOR FIRST INFECTION WITH NO PREVIOUS IMMUNITY
 clearvars;
 params = struct();
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
-full_sol = full_model("default_params_full.mat", params, @ddefullhist0, options, settings,'default0_model');
+settings.dashed = true;
+full_sol0 = full_model("default_params_full.mat", params, @ddefullhist0, options, settings,'default0_model');
 
-% contour plot for beta, k_V
-params.tspan = [0, 300];
-options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
-
-beta_vals = linspace(10^(-2), 5*10^(-2), 10);
-k_vals = linspace(0.1, 1, 10);
-death_criteria = 0.9;
-
-param_contour_plot("full", death_criteria, "default_params_full.mat", params, @ddefullhist0, options, beta_vals, k_vals, 1, 'default0_betak');
-
-%% DEFAULT RUN FOR INFECTION WITH PREVIOUS IMMUNITY -- NO B CELL COMPARTMENT
+%% RUN FOR INFECTION WITH PREVIOUS IMMUNITY -- NO B CELL COMPARTMENT
 clearvars;
 params = struct();
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
+settings.dashed = true;
 
 % parameter values for B_LL
 params.k_bll = 0; 
@@ -43,26 +34,28 @@ params.k_bea = 0;
 
 full_noB_sol = full_model("default_params_full.mat", params, @ddefullhistnoB, options, settings, 'noB_model');
 
-%% DEFAULT RUN FOR INFECTION WITH PREVIOUS IMMUNITY -- NO T CELL COMPARTMENT
+%% RUN FOR INFECTION WITH PREVIOUS IMMUNITY -- NO T CELL COMPARTMENT
 clearvars;
 params = struct();
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
+settings.dashed = true;
 
 % parameter values for T_E
 params.k_te = 0;
-params.k_tetm = 0;As
+params.k_tetm = 0;
 params.d_tepi = 0; 
 params.d_te = 0;
 params.d_tmpi = 0;
 
 full_noT_sol = full_model("default_params_full.mat", params, @ddefullhistnoT, options, settings, 'noT_model');
 
-%% DEFAULT RUN FOR INFECTION WITH PREVIOUS IMMUNITY -- NO IFN MODEL
+%% RUN FOR INFECTION WITH PREVIOUS IMMUNITY -- NO IFN MODEL
 clearvars;
 params = struct();
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
+settings.dashed = true;
 
 % parameter values for I
 params.k_i = 0;
@@ -74,10 +67,11 @@ full_noIFN_sol = full_model("default_params_full.mat", params, @ddefullhist, opt
 %% DEFAULT RUN FOR INFECTION -- IIS MODEL
 clearvars;
 params = struct();
-params.tspan = [0, 100];
+params.tspan = [0, 500];
 
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
+settings.dashed = true;
 
 % UU
 IIS_sol = IIS_model("default_params_full.mat", params, @ddeIIShist, options, settings, 'iis_model');
@@ -89,22 +83,28 @@ inset_plots("default_params_full.mat", params, @ddeIIShist, options, settings, "
 
 % SU
 params.beta = 2.05*10^(-4);
-params.b_2 = 0.2;
+params.b_2 = 0.15;
 %IIS_sol = IIS_model("default_params_full.mat", params, @ddeIIShist, options, settings);
 inset_plots("default_params_full.mat", params, @ddeIIShist, options, settings, "I zero", "iis_DF", 'iis_sim_su')
 
-% SS -- converges to I^* not 0
-params.beta = 1.5*10^(-3);
-params.b_2 = 0.9;
-%IIS_sol = IIS_model("default_params_full.mat", params, @ddeIIShist, options, settings);
-inset_plots("default_params_full.mat", params, @ddeIIShist, options, settings, "I star", "iis_DF", 'iis_sim_ss')
+% single -- converges to E
+settings.dashed = false;
+params.beta = 2.175*10^(-2);
+params.b_2 = 0.54;
+IIS_sol = IIS_model("default_params_full.mat", params, @ddeIIShist, options, settings, 'iis_sim_single_e');
 
+% single -- converges to DF
+params.tspan = [0, 300];
+params.beta = 2.175*10^(-3);
+params.b_2 = 0.54;
+inset_plots("default_params_full.mat", params, @ddeIIShist, options, settings, "I zero", "iis_DF", 'iis_sim_single_df')
 
-%% DEFAULT RUN FOR INFECTION WITH NO IMMUNE SYSTEM AT ALL -- IIS MODEL
+%% RUN FOR INFECTION WITH NO IMMUNE SYSTEM AT ALL -- IIS MODEL
 clearvars;
 params = struct();
 options = ddeset('AbsTol', 1e-9, 'RelTol', 1e-9);
 settings = struct();
+settings.dashed = true;
 
 % parameter values for I
 params.k_i = 0;
@@ -118,7 +118,7 @@ params.d_x = 10^(-1);
 params.mu = log10(5.25*10^9)*params.d_x; 
 params.beta = defaults.d_v * defaults.d_y*params.d_x/(defaults.k * params.mu);
 
-inset_plots("default_params_full.mat", params, @ddeIIShist, options, settings, "", "viral_DF", 'viral_sim_r0')
+inset_plots("default_params_full.mat", params, @ddeIIShist, options, settings, "I zero", "viral_DF", 'viral_sim_r0')
 
 
 %% HISTORY FUNCTIONS FOR VARIOUS SCENARIOS
